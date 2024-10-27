5a5364d796d0d84eabafdec09b4c888cad7d8dfe
Epharus Ndumia <epharusndumia@gmail.com>
2024-10-27 06:32:56 +0300

    coding challenge, working with && and rest operator

--- starter/script.js
+++ starter/script.js
@@ -46,31 +46,31 @@ const restaurant = {
   },
 };
 
-const rest1 = {
-  name: 'Lavida restaurant',
-  // numGuests: 10,
-  numGuests: 0,
-};
-const rest2 = {
-  name: 'Lamalina restaurant',
-  owner: 'Rossi',
-};
+// const rest1 = {
+//   name: 'Lavida restaurant',
+//   // numGuests: 10,
+//   numGuests: 0,
+// };
+// const rest2 = {
+//   name: 'Lamalina restaurant',
+//   owner: 'Rossi',
+// };
 
-// rest1.numGuests = rest1.numGuests || 5;
-// rest2.numGuests = rest2.numGuests || 5;
+// // rest1.numGuests = rest1.numGuests || 5;
+// // rest2.numGuests = rest2.numGuests || 5;
 
-// rest1.numGuests ||= 5;
-// rest2.numGuests ||= 5;
+// // rest1.numGuests ||= 5;
+// // rest2.numGuests ||= 5;
 
-rest1.numGuests ??= 5;
-rest2.numGuests ??= 5;
+// rest1.numGuests ??= 5;
+// rest2.numGuests ??= 5;
 
-// rest1.owner = rest1.owner && '<Anonymus>';
-// rest2.owner = rest2.owner && '<Anonymus>';
+// // rest1.owner = rest1.owner && '<Anonymus>';
+// // rest2.owner = rest2.owner && '<Anonymus>';
 
-rest1.owner &&= '<Anonymus>';
-rest2.owner &&= '<Anonymus>';
-console.log(rest1, rest2);
+// rest1.owner &&= '<Anonymus>';
+// rest2.owner &&= '<Anonymus>';
+// console.log(rest1, rest2);
 
 // const arr = [2, 3, 4];
 // const a = arr[0];
@@ -278,3 +278,103 @@ console.log(rest1, rest2);
 
 // const guestCorrect = restaurant.numGuests ?? 10;
 // console.log(guestCorrect);
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+console.log(game.players);
+
+// Coding Challenge #1
+// We're building a football betting app (soccer for my American friends 😅)!
+// Suppose we get data from a web service about a certain game ('game' variable on
+// next page). In this challenge we're gonna work with that data.
+// Your tasks:
+// 1. Create one player array for each team (variables 'players1' and
+// 'players2')
+const [players1, players2] = game.players;
+console.log(players1, players2);
+
+// 2. The first player in any player array is the goalkeeper and the others are field
+// players. For Bayern Munich (team 1) create one variable ('gk') with the
+// goalkeeper's name, and one array ('fieldPlayers') with all the remaining 10
+// field players
+// spead operator for the rest ...fieldPlayers gk for goalkeeper
+const [gk, ...fieldPlayers] = players1;
+console.log(gk, fieldPlayers);
+
+// 3. Create an array 'allPlayers' containing all players of both teams (22
+// players)
+const allPlayers = [...players1, ...players2];
+console.log(allPlayers);
+
+// 4. During the game, Bayern Munich (team 1) used 3 substitute players. So create a
+// new array ('players1Final') containing all the original team1 players plus
+// 'Thiago', 'Coutinho' and 'Perisic'
+const players1Final = [...players1, 'Thiago', 'Coutinho', 'Perisic'];
+console.log(players1Final);
+// // 5. Based on the game.odds object, create one variable for each odd (called
+// 'team1', 'draw' and 'team2')
+
+const {
+  odds: { team1, x: draw, team2 },
+} = game;
+console.log(team1, draw, team2);
+
+// 6. Write a function ('printGoals') that receives an arbitrary number of player
+const printGoals = function (...players) {
+  console.log(`${players.length} goals were scored`);
+};
+
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals('Davies', 'Muller');
+printGoals(...game.scored);
+
+// names (not an array) and prints each of them to the console, along with the
+// number of goals that were scored in total (number of player names passed in)
+// 7. The team with the lower odd is more likely to win. Print to the console which
+// team is more likely to win, without using an if/else statement or the ternary
+// operator.
+
+team1 < team2 && console.log('Team 1 is more likely to win');
+team1 > team2 && console.log('Team 2 is more likely to win');
+// Test data for 6.: First, use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'.
+// Then, call the function again with players from game.scored
+// GOOD LUCK 😀
